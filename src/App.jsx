3fea1182a64edcc5d2bdc77042c57aaba363784e
Ritik <rpsritik@gmail.com>
2022-02-10 15:54:38 +0530

    enabled drop and detection of dropped element

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import CustomModal from "./components/CustomModal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/App.css";
 
 function App() {
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const mainRef = useRef();
+
+  function handleDragStart(e) {
+    console.log("drag started");
+    // set the sharable data on drop event
+    e.dataTransfer.setData("element", e.target.nodeName);
+  }
+
+  useEffect(() => {
+    function handleDrop(e) {
+      // disable the browser's default actions, like opening the url, file in case it's one
+      e.preventDefault();
+      // get the sharable data
+      console.log(e.dataTransfer.getData("element"));
+    }
+    function enableDrag(e) {
+      // make the element droppable, by default most of the elements are not droppable
+      e.preventDefault();
+    }
+    mainRef.current.addEventListener("drop", handleDrop);
+    mainRef.current.addEventListener("dragover", enableDrag);
+  }, []);
   return (
     <>
       <CustomModal show={showModal} closeFunc={setShowModal} />
       <div className="app">
-        <main></main>
+        <main ref={mainRef}></main>
         <aside>
           <h1>BLOCKS</h1>
           <section>
-            <article>Label</article>
-            <article>Input</article>
-            <article>Button</article>
+            <article draggable={true} onDragStart={handleDragStart}>
+              Label
+            </article>
+            <article draggable={true} onDragStart={handleDragStart}>
+              Input
+            </article>
+            <article draggable={true} onDragStart={handleDragStart}>
+              Button
+            </article>
           </section>
         </aside>
       </div>
