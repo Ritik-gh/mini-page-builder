433ff30e657df05f22075f2daeb0c9f19d0ece36
Ritik <rpsritik@gmail.com>
2022-02-10 14:39:43 +0530

    created basic structure

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <main>
+     </main>
+        <aside>
+          <h1>BLOCKS</h1>
+          <section>
+            <article>Label</article>
+            <article>Input</article>
+            <article>Button</article>
+          </section>
+        </aside>
+    </div>
+  );
+}
+
+export default App;
